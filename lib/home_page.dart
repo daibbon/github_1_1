0fc79bbc6e0aec4628efbb34e2701c845e3d4ddb
Daisuke <91401516+daibbon@users.noreply.github.com>
2021-10-04 20:14:09 +0900

    mainのログイン、 homeのbottomNavigationBar追加

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  List<Widget> widgetList = [
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 100, left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('トレーニング',style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 30),)
+            ],
+          ),
+        ),
+      ],
+    ),
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 100, left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('設定',style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 30),)
+            ],
+          ),
+        ),
+      ],
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetList[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'トレーニング',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '設定',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (index){
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
---
+++ lib/sign_in.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:github_1/home_page.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  // Formのkeyを指定する場合は<FormState>としてGlobalKeyを定義する
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        // Formのkeyに指定する
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'TRECA',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+
+                SizedBox(height: 16),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'ニックネーム',
+                  ),
+                  keyboardType: TextInputType.name,
+                  // ニックネームのバリデーション
+                  validator: (String? value) {
+                    // ニックネームが入力されていない場合
+                    if (value?.isEmpty == true) {
+                      // 問題があるときはメッセージを返す
+                      return 'ニックネームを入力して下さい';
+                    }
+                    // 問題ないときはnullを返す
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // ログインボタンをタップしたときの処理
+                    onPressed: () => _onSignIn(),
+                    child: Text('はじめる'),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onSignIn() {
+    // 入力内容を確認する
+    if (_formKey.currentState?.validate() != true) {
+      // エラーメッセージがあるため処理を中断する
+      return;
+    }
+    // 画像一覧画面に切り替え
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) => HomePage(),
+      ),
+    );
+  }
+
+}
+
