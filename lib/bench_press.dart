3f6b8fe899877683b97dc9f5bbc230c45f46b1ce
Daisuke <91401516+daibbon@users.noreply.github.com>
2021-11-10 08:07:25 +0900

    2021/9/10 児島 13

--- lib/bench_press.dart
+++ lib/bench_press.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:github_1/bench_add.dart';
 import 'package:github_1/workout_set.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 
 
@@ -13,6 +15,13 @@ class BenchPressPage extends StatefulWidget {
   final String menuName ;
   final String menuId ;
   BenchPressPage(this.areaId,this.menuName,this.menuId);
+
+  // リストのデータ
+  final List<String> sampleList = <String>[
+    '11/1',
+    '10/30',
+    '11/29',
+  ];
 
   @override
   _BenchPressPageState createState() => _BenchPressPageState();
@@ -44,8 +53,6 @@ class _BenchPressPageState extends State<BenchPressPage> {
         .collection('posts')
         .snapshots();
   }
-  // Todoリストのデータ
-  List<String> _list = [];
 
   @override
   Widget build(BuildContext context) {
@@ -59,46 +66,54 @@ class _BenchPressPageState extends State<BenchPressPage> {
         iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: CupertinoColors.white,
       ),
-      body: ListView.builder(
-        itemCount: _list.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(_list[index]),
-              // onTap: (){
-              //   Navigator.push(context, MaterialPageRoute(builder: (context)=>ChestPage()));
-              // },
-            ),
-          );
-        },
-      ),
-      floatingActionButton:
-      FloatingActionButton.extended(
-        icon: const Icon(Icons.add),
-        label: const Text('セットを追加'),
-        backgroundColor: Colors.orange,
-        foregroundColor: Colors.white,
-        elevation: 0.5,
-        onPressed: () async {
-          // "push"で新規画面に遷移
-          // メニュー追加画面から渡される値を受け取る
-          final newListText = await Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              // 遷移先の画面としてメニュー追加画面を指定
-              return BenchAddPage();
-            }),
-          );
-          if (newListText != null) {
-            // キャンセルした場合は newListText が null となるので注意
-            setState(() {
-              // リスト追加
-              _list.add(newListText);
-            });
-          }
-        },
-        // child: Icon(
-        //     Icons.add, size: 36),
-      ),
+        body: Container(
+            margin: EdgeInsets.fromLTRB(0, 24, 0, 6),
+            decoration: BoxDecoration(color: Color(0xFFffffff)),
+            child: Expanded(
+              child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return InkWell(
+                    // onTap: (){
+                    //   Navigator.push(context, MaterialPageRoute(builder: (context)=>Setmanege()));
+                    // },
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(18, 6, 18, 6),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFFbdbdbd)),
+                        borderRadius: BorderRadius.circular(12),
+                        // color: Color(0xFF424242).withOpacity(1),
+                      ),
+                      // color: Color(value),
+                      child: Padding(
+                        child: Text(
+                            '${widget.sampleList[index]}',
+                            style: GoogleFonts.notoSans(
+                              textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16.0,color: Color(0xFF000000).withOpacity(0.7)),
+                            )
+                        ),
+                        padding: EdgeInsets.all(20.0),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: widget.sampleList.length,
+              ),
+            )),
+
+        floatingActionButton:
+        FloatingActionButton(
+          backgroundColor: const Color(0xFFffffff),
+          foregroundColor: Color(0xFFFFCC80),
+          onPressed: () {
+            // Navigator.push(context, MaterialPageRoute(builder: (context)=>SetAdd2()));
+            showMaterialModalBottomSheet(
+              context: context,
+              builder: (context) => BenchAddPage(),
+            );
+          },
+          child: Icon(Icons.add,size: 44,),
+        )
     );
   }
 }
